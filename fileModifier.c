ee0eb11acb07b2640f4a11941405d168b3e711d2
Alexandru Coderie <alex@DESKTOP-OT7MOHK.localdomain>
2023-03-31 12:22:04 +0300

    Implemented the regular file manager menu

--- fileModifier.c
+++ fileModifier.c
@@ -1,53 +1,148 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
 #include <fcntl.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
-void rf_menu(char *filname)
+static char * read_stdin(void)
 {
-  char input;
+  size_t cap = 4096, len = 0;
+  char *buffer = malloc(cap * sizeof(char));
+  int c;
 
-  do {
-    printf("Regular File Menu:\n\n");
+  while((c = fgetc(stdin)) != '\n' && !feof(stdin))
+  {
+    buffer[len] = c;
 
-    printf("-n File name\n");
-    printf("-d File size\n");
-    printf("-h Number of hardlinks\n");
-    printf("-m Time of last modification\n");
-    printf("-a Acces rights\n");
-    printf("-l Create a symbolic link\n");
+    if(++len == cap)
+      buffer = realloc(buffer, (cap *= 2) * sizeof(char));
+  }
 
-    switch (input) {
-      case 'n':
-        // do n
-        break; 
-      case 'd':
-        //do d
-        break;
-      case 'h':
-        //do h
-        break;
-      case 'm':
-        // do m
-        break;
-      case 'a':
-        // do a
-        break;
-      case 'l':
-        // do l
-        break;
-      case 'q':
+  buffer = realloc(buffer, (len + 1) * sizeof(char));
+  buffer[len] = '\0';
+
+  return buffer;
+}
+
+void rf_menu(char *filepath)
+{
+  char *input;
+  struct stat filestat;
+  printf("Regular File Menu:\n\n");
+
+  printf("-n File name\n");
+  printf("-d File size\n");
+  printf("-h Number of hardlinks\n");
+  printf("-m Time of last modification\n");
+  printf("-a Acces rights\n");
+  printf("-l Create a symbolic link\n");
+  printf("-q Exit\n");
+  while(1)
+  {
+    printf("Chose an option:\n");
+  
+    input = read_stdin();
+
+    for(int i = 1; input[i] != '\0'; i++)
+    {
+      if (input[i] == 'q') 
+      {
         printf("Goodbye!\n");
-        break;
+        return;
+      }
+
+      switch (input[i]) {
+       case 'n':
+          {
+            char *buff;
+            char *filename = filepath;
+            
+            if(filename[0] == '/') // if the given path is the root directory it will start with a ''
+              filename++;
+            
+            while((buff = strchr(filename, '/')) != NULL)
+              filename = buff + 1;
+            printf("File name: %s\n", filename);
+            break;
+          }
+       case 'd':
+          {
+            if((stat(filepath, &filestat)) < 0)
+            {
+              perror("fstat");
+              return;
+            }
+
+            printf("File size: %ld\n", filestat.st_size);
+            break;
+          }
+      case 'h':
+        { 
+          if((stat(filepath, &filestat)) < 0)
+          {
+            perror("fstat");
+            return;
+          }
+          
+          printf("Number of hard links: %ld\n", filestat.st_nlink);
+          break;
+        }
+      case 'm':
+        {
+          if((stat(filepath, &filestat)) < 0)
+          {
+            perror("fstat");
+            return;
+          }
+
+          printf("Time of last modification: %s", ctime(&filestat.st_mtime));
+          break;
+        }
+      case 'a':
+        { 
+          if((stat(filepath, &filestat)) < 0)
+          {
+            perror("fstat");
+            return;
+          }
+          
+          printf((filestat.st_mode & S_IRUSR) ? "r" : "-");
+          printf((filestat.st_mode & S_IWUSR) ? "w" : "-");
+          printf((filestat.st_mode & S_IXUSR) ? "x" : "-");
+          printf((filestat.st_mode & S_IRGRP) ? "r" : "-");
+          printf((filestat.st_mode & S_IWGRP) ? "w" : "-");
+          printf((filestat.st_mode & S_IXGRP) ? "x" : "-");
+          printf((filestat.st_mode & S_IROTH) ? "r" : "-");
+          printf((filestat.st_mode & S_IWOTH) ? "w" : "-");
+          printf((filestat.st_mode & S_IXOTH) ? "x\n" : "-\n");
+          break;
+        }
+      case 'l':
+        {
+          char *linkname;
+          printf("Enter the name of the symbolic link you want to create:\n");
+          linkname = read_stdin();
+          
+          if(symlink(filepath, linkname) < 0)
+          {
+            perror("fstat");
+            return;
+          }
+
+          printf("You created the symbolic link '%s' for file '%s'\n", linkname, filepath);
+          break;
+        }
       default:
         printf("Invalid choice!\n");
         break;
+      }  
     }
-  }while (input != 'q');
+  }
 }
-
-// void sl_menu(char *filename)
+// void sl_menu(char *filename
 // {
 //   char input;
 //
@@ -65,20 +160,21 @@ int main(int argc, char **argv)
 
   for(int i = 1; i < argc; i++)
   {
-    if(lstat(argv[i]), &filestat) < 0)
+    if(lstat(argv[i], &filestat) < 0)
     {
       printf("Error: Cannot lstat the file %s\n", argv[i]);
       continue;
     }
 
-    if(S_ISREG(filstat.st_mode))
+    if(S_ISREG(filestat.st_mode))
     {
       rf_menu(argv[i]);
     }
 
-    if(S_ISLINK(filestat.st_mode))
+    if(S_ISLNK(filestat.st_mode))
     {
-      sl_menu(argv[i]);
+      printf("Link: %s", argv[i]);
+      //sl_menu(argv[i]);
     }
     if(S_ISDIR(filestat.st_mode))
     {
